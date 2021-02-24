149a6a6968dbb1917c361595002e05cd5fb72b56
Michael Kollmann <michael.kollmann@presens.de>
2021-02-24 21:24:07 +0100

    Initial check-in of module spirexlsxissue

---
+++ spirexlsxissue/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Spire.Xls;
+using Xamarin.Forms;
+
+namespace spirexlsxissue
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var workbook = new Workbook();
+            var worksheet = workbook.Worksheets.Create();
+
+            worksheet[1, 1].Style.Color = Color.Black;
+        }
+    }
+}
